6a0c58a6f2dfad8a6126a49749a4784ceacf3108
Konrad Grzanek <kongra@gmail.com>
2016-10-05 09:02:18 +0200

    Restored clojure-1.8.0 dependency.

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :license     {:name "Eclipse Public License"
                 :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :profiles     {:repl {:plugins      [[lein-nodisassemble  "0.1.3"]]}
-                 :dev  {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :dependencies [[org.clojure/clojure                  "1.8.0"]]
+  :profiles     {:repl {:plugins [[lein-nodisassemble  "0.1.3"]]}}
 
   :aot          :all
 
