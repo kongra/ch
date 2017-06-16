f2100764db3fa70b8f48e382892795c9f2583d93
Konrad Grzanek <kongra@gmail.com>
2017-06-16 10:43:30 +0200

    Library update

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
 ;; Copyright (c) Konrad Grzanek
 ;; Created 2016-10-05
-(defproject kongra/ch "0.1.6"
+(defproject kongra/ch "0.1.7"
   :description "Low-cost dynamic type checks for ADTs"
   :url         "http://github.com/kongra/ch"
   :license     {:name "Eclipse Public License"
                 :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure                  "1.8.0"]
-                 [primitive-math                       "0.1.5"]]
+                 [primitive-math                       "0.1.6"]]
 
   :profiles     {:repl {:plugins [[lein-nodisassemble  "0.1.3"]]}}
 
