d97455aef367142c977fd743ac543341a574045c
kongra <kongra@gmail.com>
2018-08-25 21:28:45 +0200

    Starting to work on clojurescript version.

--- project.clj
+++ project.clj
@@ -6,8 +6,13 @@
   :license     {:name "Eclipse Public License"
                 :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [primitive-math      "0.1.6"]]
+  :dependencies [[org.clojure/clojure          "1.9.0"]
+                 [primitive-math               "0.1.6"]
+                 [org.clojure/clojurescript "1.10.339"]]
+
+  :plugins      [[lein-cljsbuild               "1.1.7"]
+                 [lein-figwheel               "0.5.16"
+                  :exclusions [org.clojure/clojure]]]
 
   :profiles     {:repl
                  {:dependencies [[org.clojure/tools.nrepl "0.2.13"]]
@@ -24,4 +29,20 @@
 
   :pedantic? :warn
 
-  :jvm-opts     ["-Dclojure.compiler.direct-linking=true"])
+  :jvm-opts     ["-Dclojure.compiler.direct-linking=true"]
+
+  :cljsbuild {
+              :builds [{:id "dev"
+                        :source-paths ["src/cljs"]
+                        :figwheel {:on-jsload            "kongra.ch/onJSreload"}
+                        :compiler {:main                 kongra.ch
+                                   :asset-path           "js/compiled/out"
+                                   :output-to            "resources/public/js/compiled/ch.js"
+                                   :output-dir           "resources/public/js/compiled/out"
+                                   :source-map-timestamp true }}
+                       {:id "min"
+                        :source-paths ["src/cljs"]
+                        :compiler {:output-to            "resources/public/js/compiled/ch.js"
+                                   :main                  kongra.ch
+                                   :optimizations         :advanced
+                                   :pretty-print          false }}]})
